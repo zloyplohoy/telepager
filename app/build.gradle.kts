4b146150966d9a47b2f3f15abc305246b619959b
Aleksei Sokolov <sokolov.ag@sokolov.ag>
2024-11-27 00:55:39 +0100

    Add and apply Android application convention plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,17 +1,13 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.telepager.android.application)
     alias(libs.plugins.kotlin.compose)
 }
 
 android {
     namespace = "ag.sokolov.telepager"
-    compileSdk = 35
 
     defaultConfig {
         applicationId = "ag.sokolov.telepager"
-        minSdk = 28
-        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -26,13 +22,6 @@ android {
                 "proguard-rules.pro"
             )
         }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
     }
     buildFeatures {
         compose = true
