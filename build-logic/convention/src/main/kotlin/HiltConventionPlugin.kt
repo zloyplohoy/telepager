16e641d06f887fbbef15797bf5f8621ea6c50000
Aleksei Sokolov <sokolov.ag@sokolov.ag>
2024-11-26 22:44:20 +0100

    Add Hilt convention plugin

---
+++ build-logic/convention/src/main/kotlin/HiltConventionPlugin.kt
@@ -0,0 +1,22 @@
+import ag.sokolov.telepager.libs
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+
+class HiltConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            pluginManager.apply("com.google.devtools.ksp")
+
+            dependencies {
+                add("ksp", libs.findLibrary("hilt.compiler").get())
+            }
+
+            pluginManager.withPlugin("org.jetbrains.kotlin.jvm") {
+                dependencies {
+                    add("implementation", libs.findLibrary("hilt.core").get())
+                }
+            }
+        }
+    }
+}
