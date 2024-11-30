235c98f5ea7e81568c80469fa08fb14e27ed46ff
Aleksei Sokolov <sokolov.ag@sokolov.ag>
2024-11-30 06:29:25 +0100

    Reorder imports in build logic

--- build-logic/convention/src/main/kotlin/AndroidApplicationComposeConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidApplicationComposeConventionPlugin.kt
@@ -1,5 +1,5 @@
-import com.android.build.api.dsl.ApplicationExtension
 import ag.sokolov.telepager.configureAndroidCompose
+import com.android.build.api.dsl.ApplicationExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.apply
--- build-logic/convention/src/main/kotlin/ag/sokolov/telepager/KotlinAndroid.kt
+++ build-logic/convention/src/main/kotlin/ag/sokolov/telepager/KotlinAndroid.kt
@@ -6,8 +6,8 @@ import org.gradle.api.Project
 import org.gradle.api.plugins.JavaPluginExtension
 import org.gradle.kotlin.dsl.assign
 import org.gradle.kotlin.dsl.configure
-import org.gradle.kotlin.dsl.provideDelegate
 import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.provideDelegate
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
