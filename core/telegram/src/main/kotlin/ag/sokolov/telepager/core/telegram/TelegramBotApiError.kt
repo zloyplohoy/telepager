573223e1f76a0f8e338bfce5ced8724c2cab1372
Aleksei Sokolov <sokolov.ag@sokolov.ag>
2024-12-24 20:26:34 +0100

    Add a sealed Telegram bot API error class

---
+++ core/telegram/src/main/kotlin/ag/sokolov/telepager/core/telegram/TelegramBotApiError.kt
@@ -0,0 +1,19 @@
+package ag.sokolov.telepager.core.telegram
+
+sealed class TelegramBotApiError {
+    // Generic errors
+    data class NetworkError(val message: String?) : TelegramBotApiError()
+    data class UnknownError(val message: String?) : TelegramBotApiError()
+
+    // Bot API errors
+    data object Unauthorized : TelegramBotApiError()
+
+    sealed class Forbidden : TelegramBotApiError() {
+        data object BotBlocked : Forbidden()
+    }
+
+    sealed class BadRequest : TelegramBotApiError() {
+        data object MessageTextIsEmpty : BadRequest()
+        data object ChatNotFound : BadRequest()
+    }
+}
