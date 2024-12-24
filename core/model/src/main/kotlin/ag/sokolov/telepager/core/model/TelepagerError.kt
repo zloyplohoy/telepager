b56dba0365456ab90c4197c060ef9336508c62c3
Aleksei Sokolov <sokolov.ag@sokolov.ag>
2024-12-24 20:29:27 +0100

    Remove deprecated result and error models

--- core/model/src/main/kotlin/ag/sokolov/telepager/core/model/TelepagerError.kt
+++
@@ -1,7 +0,0 @@
-package ag.sokolov.telepager.core.model
-
-sealed class TelepagerError(open val message: String?) {
-    data class NetworkError(override val message: String?) : TelepagerError(message)
-    data class BotError(override val message: String?) : TelepagerError(message)
-    data class UnknownError(override val message: String?) : TelepagerError(message)
-}
--- core/model/src/main/kotlin/ag/sokolov/telepager/core/model/TelepagerResult.kt
+++
@@ -1,7 +0,0 @@
-package ag.sokolov.telepager.core.model
-
-sealed class TelepagerResult<out T, out E> {
-    data class Success<out T>(val data: T? = null) : TelepagerResult<T, Nothing>()
-    data class Failure<out E>(val error: E) : TelepagerResult<Nothing, E>()
-    data object Loading : TelepagerResult<Nothing, Nothing>()
-}
