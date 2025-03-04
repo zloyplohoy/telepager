98099ad83a6f29db186b1c5efecb121998e51ed0
Aleksei Sokolov <sokolov.ag@sokolov.ag>
2025-03-04 22:05:18 +0100

    Add Ktor HTTP client and its dependencies to Telegram module

--- core/telegram/build.gradle.kts
+++ core/telegram/build.gradle.kts
@@ -11,6 +11,11 @@ dependencies {
     implementation(projects.core.model)
 
     implementation(libs.kotlinx.serialization.json)
+    implementation(libs.ktor.client.cio)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.resources)
+    implementation(libs.ktor.serialization.kotlinx.json)
     implementation(libs.okhttp)
     implementation(libs.retrofit)
     implementation(libs.retrofit.converter.kotlinx.serialization)
