5a593ef35eb317e30c404c5d981de0999d32057b
Aleksei Sokolov <sokolov.ag@sokolov.ag>
2025-03-05 06:22:36 +0100

    Remove unused core.concurrency dependency from home feature module

--- feature/home/build.gradle.kts
+++ feature/home/build.gradle.kts
@@ -8,7 +8,6 @@ android {
 }
 
 dependencies {
-    implementation(projects.core.concurrency)
     implementation(projects.core.data)
     implementation(projects.core.designsystem)
     implementation(projects.core.domain)
